7ee94ff55bf2572a11622af0033d20885e8b6adf
AgnethaDMello <agnethad@nycstudents.net>
2021-10-28 13:31:07 -0400

    Final Edit

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const Madame = {
     name: "Lady Mademoiselle",
-    height: "175 cm",
-    weight: "61 kg"
+    height: "175",
+    weight: "61"
+}
+
+function bmiCalc(Madame) {
+    return Madame.weight / (Madame.height / 100) ** 2;
+}
+
+function range(Madame) {
+    const BMI = bmiCalc(Madame);
+    if (BMI < 18.5) {
+        return "Underweight"
+    }
+    else if (BMI >= 18.5 && BMI < 25) {
+        return "Healthy"
+    }
+    else {
+        return "Overweight"
+    }
 }
 
 console.log(Madame)
-
-function bmicalc(Madame) {
-    return Madame.weight / (Madame.height / 100) ** 2
-}
-console.log(bmicalc(Madame))
+console.log(`BMI is ${bmiCalc(Madame)}`)
+console.log(range(Madame))
